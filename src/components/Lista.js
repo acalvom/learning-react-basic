94c7adc36923e9c98a597b4e809377af916b3899
acalvom <andreac.3108@gmail.com>
2021-05-09 19:10:26 +0200

    Añadir elementos a un array utilizando Hooks

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -2,12 +2,22 @@ import React, {useState, Fragment} from "react";
 
 const Lista = () => {
     const [arrayNumero, setArrayNumero] = useState([1, 2, 3, 4, 5]);
+    const [numero, setNumero] = useState(arrayNumero.length + 1);
+
+    const AgregarElemento = () => {
+        setNumero(numero + 4); // Utilizamos hooks para modificar el valor de un elemento
+        setArrayNumero([
+            ...arrayNumero, // Para concatenar arrays se utilizan ...
+            numero
+        ])
+    }
 
     return (
         <Fragment>
             <h2>Lista de números</h2>
+            <button onClick={AgregarElemento}>Añadir</button>
             {
-                /* Cuando se quieren obtener los datos de un array tenemos que pasarle
+                /* Cuando se quieren obtener los datos de un array tenemos que pasar
                 el índice a cada etiqueta html: key=index*/
                 arrayNumero.map((num, index) =>
                     <ul key={index}>Número {num} - Índice {index}</ul>
