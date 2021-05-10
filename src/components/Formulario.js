0781235db8a933c66df7191d48f7c96731807836
acalvom <andreac.3108@gmail.com>
2021-05-10 08:36:52 +0200

    Formulario con Hooks

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -6,7 +6,7 @@ const Formulario = () => {
        2-En useState pasar un objeto con los nombres de la etiqueta name vacíos, que deben coincidir con los campos del formulario
        3-En los campos del input, añadir el onChange con una función que maneje el evento: event.target.value
        4-En la función onChange dar el valor a los datos con useState concatenando a lo que ya hay en el estado inicial
-       5-
+       5-Añadir la función onSubmit para enviar los datos del formulario
      */
 
     const [datos, setDatos] = useState({ // En el formulario, los datos recogidos son objetos
@@ -14,11 +14,12 @@ const Formulario = () => {
         apellido: '',
         email: ''
     });
-    const EnviarFormulario = () => {
-        console.log('Datos formulario')
+    const EnviarFormulario = (event) => {
+        event.preventDefault(); // Para evitar el procesamiento automático del formulario que se recargue la página
+        console.log(datos.nombre + ' ' + datos.apellido + ' ' + datos.email)
     }
     const ControladorCambios = (event) => {
-        console.log(event.target.name + ' ' + event.target.value)
+        //console.log(event.target.name + ' ' + event.target.value)
         setDatos({ // Como los datos del form van en un objeto se ponen {}
             ...datos,   // Igual que un array, para no perder los datos, concatenamos con lo anterior
             [event.target.name]: event.target.value // Esto es notación propia de EcmaScript 6
@@ -28,7 +29,7 @@ const Formulario = () => {
     return (
         <Fragment>
             <h2>Formulario</h2>
-            <form>
+            <form onSubmit={EnviarFormulario}>
                 <div className="row">
                     <div className="col-md-3">
                         <input type="text"
@@ -52,8 +53,11 @@ const Formulario = () => {
                                onChange={ControladorCambios}/>
                     </div>
                     <div className="col-md-3">
-                        <button onClick={EnviarFormulario} type="submit" className="btn btn-primary">Enviar</button>
+                        <button type="submit" className="btn btn-primary">Enviar</button>
                     </div>
+                </div>
+                <div className="row my-3">
+                    <h5>Tu nombre es {datos.nombre} {datos.apellido} y tu email {datos.email}</h5>
                 </div>
             </form>
         </Fragment>
